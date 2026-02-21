0ce3ccb40b4ba95725f0f1eb668c35119b85dbf1
heetsolankiii <solankiheet456@gmail.com>
2026-02-21 23:36:45 +0530

    Modified App.js

--- frontend/lawassit/src/App.js
+++ frontend/lawassit/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
+import Categories from "./components/Categories";
+import Footer from "./components/Footer";
+
+import useScrollReveal from "./hooks/useScrollReveal";
 
 function App() {
+  useScrollReveal();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Categories />
+      <Footer />
+    </>
   );
 }
 
