17adef63d1bd644327bdf08458093f65b321e943
heetsolankiii <solankiheet456@gmail.com>
2026-02-22 21:20:49 +0530

    Added Routings

--- frontend/lawassit/src/App.js
+++ frontend/lawassit/src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
