7fe6f05d711aad8e11e3faf79fdd07345122495c
heetsolankiii <solankiheet456@gmail.com>
2026-03-02 21:25:36 +0530

    Changed some configs

--- frontend/lawassit/tailwind.config.js
+++ frontend/lawassit/tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+  extend: {
+    animation: {
+      fadeIn: "fadeIn 0.3s ease-out",
+    },
+    keyframes: {
+      fadeIn: {
+        "0%": { opacity: 0, transform: "scale(0.95)" },
+        "100%": { opacity: 1, transform: "scale(1)" },
+      },
+    },
+  },
+};
