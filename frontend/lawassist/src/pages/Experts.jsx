705e5f8be39b52013e9955f221ff082add0c1414
heetsolankiii <solankiheet456@gmail.com>
2026-03-07 12:44:34 +0530

    Minor update

--- frontend/lawassist/src/pages/Experts.jsx
+++ frontend/lawassist/src/pages/Experts.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { MapPin, Star, Briefcase, IndianRupee } from "lucide-react";
+import { MapPin, Briefcase, IndianRupee } from "lucide-react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import BackToTopButton from "../components/BackToTopButton";
