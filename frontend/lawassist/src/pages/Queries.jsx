1668621ccc877034486faf61a8958f61547cdd49
heetsolankiii <solankiheet456@gmail.com>
2026-03-08 09:15:17 +0530

    Changed API URL

--- frontend/lawassist/src/pages/Queries.jsx
+++ frontend/lawassist/src/pages/Queries.jsx
@@ -8,7 +8,7 @@ import BackToTopButton from "../components/BackToTopButton";
 import AlertPopup from "../components/AlertPopup";
 import { categories, getStatusClass } from "../data";
 
-const API = "https://lawassist-api.onrender.com/api";
+const API = "https://law-assist.onrender.com/api";
 
 const Queries = () => {
   const [selectedCategory, setSelectedCategory] = useState("All");
