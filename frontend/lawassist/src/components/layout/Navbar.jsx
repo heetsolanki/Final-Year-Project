1097d8a986eb2b44fba79a8ce14cb29410d13186
heetsolankiii <solankiheet456@gmail.com>
2026-03-15 09:17:00 +0530

    Enhance Navbar component to display user name and improve authentication handling

--- frontend/lawassist/src/components/layout/Navbar.jsx
+++ frontend/lawassist/src/components/layout/Navbar.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import { Scale, Menu, X } from "lucide-react";
+import { Scale, Menu, X, User2Icon } from "lucide-react";
 import { Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
+import API_URL from "../../api";
+import axios from "axios";
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,9 +12,10 @@ function Navbar() {
   const location = useLocation();
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userRole, setUserRole] = useState(null);
+  const [userName, setUserName] = useState("");
 
   useEffect(() => {
-    const checkAuth = () => {
+    const checkAuth = async () => {
       const token = localStorage.getItem("token");
 
       if (token) {
@@ -20,18 +23,32 @@ function Navbar() {
           const decoded = jwtDecode(token);
           setIsLoggedIn(true);
           setUserRole(decoded.role);
+          if (userRole === "consumer") {
+            const res = await axios.get(`${API_URL}/api/users/profile`, {
+              headers: { Authorization: `Bearer ${token}` },
+            });
+
+            setUserName(res.data.name);
+          } else if (userRole === "legalExpert") {
+            const res = await axios.get(`${API_URL}/api/expert/profile`, {
+              headers: { Authorization: `Bearer ${token}` },
+            });
+            setUserName(res.data.name);
+          }
         } catch {
           setIsLoggedIn(false);
           setUserRole(null);
+          setUserName("");
         }
       } else {
         setIsLoggedIn(false);
         setUserRole(null);
+        setUserName("");
       }
     };
 
     checkAuth();
-  }, [location]);
+  }, [location, userRole]);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -80,6 +97,7 @@ function Navbar() {
 
         {/* RIGHT LINKS */}
         <div className="nav-links">
+          {userRole === "consumer" && (
           <Link
             to="/experts"
             className="nav-link"
@@ -87,6 +105,7 @@ function Navbar() {
           >
             Experts
           </Link>
+          )}
           <Link
             to="/about"
             className="nav-link"
@@ -117,23 +136,25 @@ function Navbar() {
               {userRole === "consumer" && (
                 <Link
                   to="/user-dashboard"
-                  className="nav-btn nav-login"
+                  className="nav-login px-3 py-2 flex items-center align-center gap-1 rounded-full border border-[#0A1F44] hover:bg-[#0A1F44] hover:text-white transition"
                   onClick={() =>
                     window.scrollTo({ top: 0, behavior: "smooth" })
                   }
                 >
-                  My Dashboard
+                  <User2Icon size={18} />
+                  <span className="text-sm font-medium">{userName}</span>
                 </Link>
               )}
               {userRole === "legalExpert" && (
                 <Link
                   to="/legal-expert-dashboard"
-                  className="nav-btn nav-login"
+                  className="nav-login px-3 py-2 flex items-center align-center gap-1 rounded-full border border-[#0A1F44] hover:bg-[#0A1F44] hover:text-white transition"
                   onClick={() =>
                     window.scrollTo({ top: 0, behavior: "smooth" })
                   }
                 >
-                  Expert Dashboard
+                  <User2Icon size={18} />
+                  <span className="text-sm font-medium">{userName}</span>
                 </Link>
               )}
               <button className="nav-btn nav-register" onClick={handleLogout}>
@@ -181,16 +202,18 @@ function Navbar() {
         >
           Queries
         </Link>
-        <Link
-          to="/experts"
-          className="nav-link"
-          onClick={() => {
-            setIsOpen(false);
-            window.scrollTo({ top: 0, behavior: "smooth" });
-          }}
-        >
-          Experts
-        </Link>
+        {userRole === "consumer" && (
+          <Link
+            to="/experts"
+            className="nav-link"
+            onClick={() => {
+              setIsOpen(false);
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+          >
+            Experts
+          </Link>
+        )}
         <Link
           to="/about"
           className="nav-link"
